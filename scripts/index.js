fd0be979ea343ff63a4aa100a68323869a2929fc
pauloWeslleY <weslleyspfc13@outlook.com>
2022-07-10 21:10:48 -0300

    aula 99: estilização do cardápio bug(Imagem não abre)

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 $(function () {
 
-   //* Effect Smoke fro Banner
+   //* Effect Smoke for Banner
    $(selector).ripples({
       dropRadius: 25,
       perturbance: 0.2,
@@ -9,6 +9,15 @@ $(function () {
    //!BTN TOGGLE
    $('.navbar-toggler').on(function () {
       $('.navbar-toggler').toggleClass('change')
-   })
+   });
 
+   //!MagnificPopup => click img
+   $(".parent-container").magnificPopup({
+      delegate: 'a', // the selector for gallery item
+      type: 'image',
+
+      gallery: {
+        enabled: true
+      }
+   });
 })
