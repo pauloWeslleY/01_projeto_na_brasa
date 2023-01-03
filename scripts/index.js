ff947fff2e71096f2dcb94df573aa840a014d90e
pauloWeslleY <weslleyspfc@13outlook.com>
2023-01-03 18:03:26 -0300

    AULA: 99 -> Estilização do Cardápio

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,53 @@
 $(function () {
-
    //* Effect Smoke for Banner
-   $(selector).ripples({
-      dropRadius: 25,
-      perturbance: 0.2,
-    });
+   // $(selector).ripples({
+   //    dropRadius: 25,
+   //    perturbance: 0.2,
+   // });
 
    //!BTN TOGGLE
-   $('.navbar-toggler').on(function () {
-      $('.navbar-toggler').toggleClass('change')
+   $(".navbar-toggler").on(function () {
+      $(".navbar-toggler").toggleClass("change");
    });
+});
 
-   //!MagnificPopup => click img
+$(function () {
+   //!MagnificPopup => click image
    $(".parent-container").magnificPopup({
-      delegate: 'a', // the selector for gallery item
-      type: 'image',
-
+      delegate: "a", // the selector for gallery item
+      type: "image",
       gallery: {
-        enabled: true
-      }
+         enabled: true,
+      },
    });
-})
+});
+
+$(function () {
+   //!Slick Carousel
+   $(".carousel").slick({
+      centerMode: true,
+      mobileFirst: true,
+      centerPadding: "60px",
+      slidesToShow: 3,
+      responsive: [
+         {
+            breakpoint: 768,
+            settings: {
+               arrows: false,
+               centerMode: true,
+               centerPadding: "40px",
+               slidesToShow: 3,
+            },
+         },
+         {
+            breakpoint: 480,
+            settings: {
+               arrows: false,
+               centerMode: true,
+               centerPadding: "40px",
+               slidesToShow: 1,
+            },
+         },
+      ],
+   });
+});
